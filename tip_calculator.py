a9b6980cca97d5dc133226e8335fefb78902b402
tummytucker94 <52971811+tummytucker94@users.noreply.github.com>
2020-12-05 18:01:05 -0500

    Update tip_calculator.py

--- tip_calculator.py
+++ tip_calculator.py
@@ -1,9 +1,9 @@
 print("What is the bill amount?")
-bill_amount = int(input("Enter the bill amount"))
+bill_amount = float(input("Enter the bill amount"))
 print("$", bill_amount)
 
 print("What is the tip rate?")
-tip_rate = int(input("Enter tip rate?"))
+tip_rate = float(input("Enter tip rate?"))
 print(tip_rate,"%")
 
 tip = round(bill_amount * (tip_rate / 100), 2)
